c77f2d601a6c9af3df33397ff3b52c3acc0b5203
marcostanley <commercial@umail.uz>
2017-10-03 23:02:07 +0500

    Update custom.js

--- www/js/custom.js
+++ www/js/custom.js
@@ -1,94 +1,85 @@
 $(document).ready(function() {
 
-	document.addEventListener("deviceready", onDeviceReady, false);
-
 	var url;
 
-	function onDeviceReady() {
+	$('.homepage').on('click', function(e) {
 
-		$('.homepage').on('click', function(e) {
+		url = "https://motormoney.org/";
 
-			url = "https://motormoney.org/";
+		showHelp(url);
 
-			showHelp(url);
+	});
 
-		});
+	$('.login').on('click', function(e) {
 
-		$('.login').on('click', function(e) {
+		url = "https://motormoney.org/user/";
 
-			url = "https://motormoney.org/user/";
+		showHelp(url);
 
-			showHelp(url);
+	});
 
-		});
+	function showHelp(url) {
 
-		function showHelp(url) {
+	    var target = "_blank";
 
-		    var target = "_self";
+	    var options = "location=yes,hidden=yes";
 
-		    var options = "location=yes,hidden=yes";
+	    inAppBrowserRef = window.open(url, target, options);
 
-		    inAppBrowserRef = window.open(url, target, options);
+	    inAppBrowserRef.addEventListener('loadstart', loadStartCallBack);
 
-		    inAppBrowserRef.addEventListener('loadstart', loadStartCallBack);
+	    inAppBrowserRef.addEventListener('loadstop', loadStopCallBack);
 
-		    inAppBrowserRef.addEventListener('loadstop', loadStopCallBack);
-
-		    inAppBrowserRef.addEventListener('loaderror', loadErrorCallBack);
-
-		}
-
-		function loadStartCallBack() {
-
-		    $('#loaders').append('<div class="loading">'+
-				'<span>Loading</span>'+
-				'<div class="loader"></div>'+
-				'</div>');
-
-		}
-
-		function loadStopCallBack() {
-
-		    if (inAppBrowserRef != undefined) {
-
-		        inAppBrowserRef.insertCSS({ code: "body{font-size: 25px;}" });
-
-		        $('.loading').remove();
-
-		        inAppBrowserRef.show();
-		    }
-
-		}
-
-		function loadErrorCallBack(params) {
-
-		    $('#status-message').text("");
-
-		    var scriptErrorMesssage = '<div class="error-msg"><i class="fa fa-times-circle"></i>\n' + 
-		    'Sorry we cannot open that page. Message from the server is : '+ params.message +'</div>';
-
-		    inAppBrowserRef.executeScript({ code: scriptErrorMesssage }, executeScriptCallBack);
-
-		    inAppBrowserRef.close();
-
-		    inAppBrowserRef = undefined;
-
-		}
-
-		function executeScriptCallBack(params) {
-
-		    if (params[0] == null) {
-
-		        $('#loaders').append(
-		           '<div class="error-msg"><i class="fa fa-times-circle"></i>\n' + 
-		    		'Sorry we cannot open that page. Message from the server is : '+ params.message +'</div>');
-		    }
-
-		}
+	    inAppBrowserRef.addEventListener('loaderror', loadErrorCallBack);
 
 	}
 
+	function loadStartCallBack() {
 
-		
+	    $('#loaders').append('<div class="loading">'+
+			'<span>Loading</span>'+
+			'<div class="loader"></div>'+
+			'</div>');
+
+	}
+
+	function loadStopCallBack() {
+
+	    if (inAppBrowserRef != undefined) {
+
+	        inAppBrowserRef.insertCSS({ code: "body{font-size: 25px;}" });
+
+	        $('.loading').remove();
+
+	        inAppBrowserRef.show();
+	    }
+
+	}
+
+	function loadErrorCallBack(params) {
+
+	    $('#status-message').text("");
+
+	    var scriptErrorMesssage = '<div class="error-msg"><i class="fa fa-times-circle"></i>\n' + 
+	    'Sorry we cannot open that page. Message from the server is : '+ params.message +'</div>';
+
+	    inAppBrowserRef.executeScript({ code: scriptErrorMesssage }, executeScriptCallBack);
+
+	    inAppBrowserRef.close();
+
+	    inAppBrowserRef = undefined;
+
+	}
+
+	function executeScriptCallBack(params) {
+
+	    if (params[0] == null) {
+
+	        $('#loaders').append(
+	           '<div class="error-msg"><i class="fa fa-times-circle"></i>\n' + 
+	    		'Sorry we cannot open that page. Message from the server is : '+ params.message +'</div>');
+	    }
+
+	}
 
 });
