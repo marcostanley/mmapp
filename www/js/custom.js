651fd63a31141f05ba38cd092a31f7d1b1b6ddb6
marcostanley <commercial@umail.uz>
2017-10-04 15:06:18 +0500

    Update custom.js

--- www/js/custom.js
+++ www/js/custom.js
@@ -1,10 +1,7 @@
 $(document).ready(function() {
 
-	var url;
-
 	$('.homepage').click(function(e) {
 
-// 		url = "https://motormoney.org/";
 		var ref = cordova.InAppBrowser.open('https://motormoney.org/', '_blank', 'location=yes');
 		ref.addEventListener('loadstart', loadstartCallback);
 		ref.addEventListener('loadstop', loadstopCallback);
@@ -27,83 +24,32 @@ $(document).ready(function() {
 		  console.log('Browser is closed...')
 		}
 
-// 		showHelp(url);
-
 	});
 
-	$('.login').click(function(e) {
-
-		url = "https://motormoney.org/user/";
-
-		showHelp(url);
-
-	});
-
-	function showHelp(url) {
-
-	    var target = "_blank";
-
-	    var options = "location=yes,hidden=yes";
-
-	    inAppBrowserRef = window.open(url, target, options);
-
-	    inAppBrowserRef.addEventListener('loadstart', loadStartCallBack);
-
-	    inAppBrowserRef.addEventListener('loadstop', loadStopCallBack);
-
-	    inAppBrowserRef.addEventListener('loaderror', loadErrorCallBack);
-
-	}
-
-	function loadStartCallBack() {
-
-	    $('#loaders').append('<div class="loading">'+
-			'<span>Loading</span>'+
-			'<div class="loader"></div>'+
-			'</div>');
-
-	}
-
-	function loadStopCallBack() {
-
-	    if (inAppBrowserRef != undefined) {
-
-	        inAppBrowserRef.insertCSS({ code: "body{font-size: 25px;}" });
-
-	        $('.loading').remove();
-
-	        inAppBrowserRef.show();
-	    }
-
-	}
-
-	function loadErrorCallBack(params) {
-
-	    $('#status-message').text("");
-
-	    var scriptErrorMesssage = '<div class="error-msg"><i class="fa fa-times-circle"></i>\n' + 
-	    'Sorry we cannot open that page. Message from the server is : '+ params.message +'</div>';
-
-	    inAppBrowserRef.executeScript({ code: scriptErrorMesssage }, executeScriptCallBack);
-
-	    inAppBrowserRef.close();
-
-	    inAppBrowserRef = undefined;
-
-	}
-
-	function executeScriptCallBack(params) {
-
-	    if (params[0] == null) {
-
-	        $('#loaders').append(
-	           '<div class="error-msg"><i class="fa fa-times-circle"></i>\n' + 
-	    		'Sorry we cannot open that page. Message from the server is : '+ params.message +'</div>');
-	    }
-
-	}
-
-
+	$('.login').click(function() {
 		
+		var ref = cordova.InAppBrowser.open('https://motormoney.org/forms/login/', '_blank', 'location=yes');
+		ref.addEventListener('loadstart', loadstartCallback);
+		ref.addEventListener('loadstop', loadstopCallback);
+		ref.addEventListener('loadloaderror', loaderrorCallback);
+		ref.addEventListener('exit', exitCallback);
+
+		function loadstartCallback(event) {
+		  console.log('Loading started: '  + event.url)
+		}
+
+		function loadstopCallback(event) {
+		  console.log('Loading finished: ' + event.url)
+		}
+
+		function loaderrorCallback(error) {
+		  console.log('Loading error: ' + error.message)
+		}
+
+		function exitCallback() {
+		  console.log('Browser is closed...')
+		}
+		
+	});
 
 });
